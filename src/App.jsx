d3339ed63c320bbbdedf4b6800eab85457a5abe5
nikakakbexs <kakabadzenikanika@gmail.com>
2024-09-27 00:51:17 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  let firstName = "Nika";
+  return (
+    <div className="square">
+      <h1 className="title">კაკაბაძე {firstName} </h1>
+      <h1 className="ball">კალათბურთი</h1>
+    </div>
+  );
+}
+
+export default App;
